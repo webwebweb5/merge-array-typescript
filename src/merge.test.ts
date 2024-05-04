e012e643fe50011666b92411630e8d9bc07785a9
webwebweb5 <phiriyakornman@gmail.com>
2024-05-04 16:23:18 +0700

    Update test collection

--- src/merge.test.ts
+++ src/merge.test.ts
@@ -2,9 +2,9 @@ import { merge } from "./merge";
 
 describe("merge function", () => {
   it("should merge and sort three collections", () => {
-    const collection1 = [10, 1, 3, 9];
-    const collection2 = [6, 4, 11, 7];
-    const collection3 = [2, 8, 5, 12];
+    const collection1 = [1, 3, 9, 10];
+    const collection2 = [11, 7, 6, 4];
+    const collection3 = [2, 5, 8, 12];
     const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
     const result = merge(collection1, collection2, collection3);
